1c9f2ce639189dcd95365a30af996e4f34891db7
秋雨落 <i@rain.cx>
2021-05-03 10:00:54 +0800

    :sparkles: Add main class
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/games/moegirl/fortyyears/FortyYears.java
@@ -0,0 +1,12 @@
+package games.moegirl.fortyyears;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FortyYears.MODID)
+public class FortyYears {
+    public static final String MODID = "fortyyears";
+
+    public FortyYears() {
+        
+    }
+}
